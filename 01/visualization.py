e34678a3d248022672aff8b3254f23f68190b64e
yl51jowa <yl51jowa@studserv.uni-leipzig.de>
2023-11-02 13:12:53 +0100

    dash anwendung

--- 01/visualization.py
+++ 01/visualization.py
@@ -1,7 +1,33 @@
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
-import csv
 import pandas as pd
+app = Dash(__name__)
 
 
-df = pd.read_csv('ivda/01/data/Aufgabe-1.csv', on_bad_lines='skip')
-px.line(df,x="'Age'", y="'Wage(in Euro)'")
+app.layout = html.Div([
+    html.H4('Interactive scatter plot with Our dataset'),
+    dcc.Graph(id="scatter-plot"),
+    html.P("Filter by petal width:"),
+    dcc.RangeSlider(
+        id='range-slider',
+        min=0, max=60, step=1,
+        marks={10:'10',15: '15',20:'20',25: '25',30:'30',35: '35',40:'40',45: '45'},
+        value=[10, 45]
+    ),
+])
+
+
+@app.callback(
+    Output("scatter-plot", "figure"), 
+    Input("range-slider", "value"))
+def update_bar_chart(slider_range):
+    df = pd.read_csv('ivda/01/data/Aufgabe-1.csv', on_bad_lines='skip')
+    low, high = slider_range
+    mask = (df['\'Age\''] > low) & (df['\'Age\''] < high)
+    fig = px.scatter(
+        df[mask], x='\'Age\'', y="\'Overall\'", size='\'Overall\'',color='\'Wage(in Euro)\'',
+        hover_data=['\'Age\''])
+    return fig
+
+
+app.run_server(debug=True)
