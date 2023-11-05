f19355909465aa5e501860474ce4dad4e84cea1b
yl51jowa <yl51jowa@studserv.uni-leipzig.de>
2023-11-05 17:00:37 +0100

    c halbwegs fertig

--- 01/visualization.py
+++ 01/visualization.py
@@ -3,61 +3,112 @@ from dash.exceptions import PreventUpdate
 import plotly.express as px
 import pandas as pd
 
+df = pd.read_csv("ivda/01/data/Aufgabe-1.csv", on_bad_lines="skip",low_memory=False)
 app = Dash(__name__)
-hover_data = ["'Full Name'", "'Wage(in Euro)'", "'Overall'"]
+hover_data = ["'Full Name'", "'Wage(in Euro)'", "'Overall'", "'Nationality'"]
+nationalities = df["\'Nationality\'"]
+clubs = df["\'Club Name\'"]
 app.layout = html.Div(
     [
         html.H4("Interactive scatter plot with the dataset"),
         html.Div(
             [
-                "Attribut",
+                "Attribute",
                 dcc.Dropdown(
                     ["'Wage(in Euro)'", "'Overall'"],
-                    value="Overall",
-                    id="my-input",
+                    value="'Overall'",
+                    id="attribute",
                     clearable=False,
+                ),"Filter Nationality",
+                dcc.Dropdown(
+                    nationalities.to_list(),
+                    id="filter_nationality",
+                    multi=True
+                ),"Filter Club Name",
+                dcc.Dropdown(
+                    clubs.to_list(),
+                    id="filter_club",
+                    multi=True
                 ),
+                "Verteilung",
                 dcc.Graph(id="dist-plot"),
+                "Gegenueberstellung",
                 dcc.Graph(id="scatter-plot"),
             ]
-        ),
+        )
     ]
 )
 
-
 @app.callback(
     Output("scatter-plot", "figure"),
-    Input(component_id="my-input", component_property="value"),
+    Input("attribute", "value"),
+    Input("filter_nationality", "value"),
+    Input("filter_club", "value"),
 )
-def update_bar_chart(input_value):
-    df = pd.read_csv("ivda/01/data/Aufgabe-1.csv", on_bad_lines="skip")
-    try:
-        fig = px.scatter(df, x="'Age'", y=input_value, hover_data=hover_data)
-    except:
-        pass
-    return fig
+def update_scatter_chart(input_value,filter_nationality,filter_club):
+    
+    df = pd.read_csv("ivda/01/data/Aufgabe-1.csv", on_bad_lines="skip",low_memory=False)
+    color = None
+    if filter_nationality:
+        df = df[df["\'Nationality\'"].isin(filter_nationality)]
+        color="'Nationality'"
+    if filter_club:
+        df = df[df["\'Club Name\'"].isin(filter_club)]
+        color="'Club Name'"
+    if not input_value:
+        raise PreventUpdate
+    return px.scatter(df, x="'Age'", y=input_value, hover_data=hover_data, color=color)
 
 
 @app.callback(
     Output("dist-plot", "figure"),
-    Input(component_id="my-input", component_property="value"),
+    Input("attribute", "value"),
+    Input("filter_nationality", "value"),
+    Input("filter_club", "value"),
 )
-def update_bar_chart(input_value):
-    df = pd.read_csv("ivda/01/data/Aufgabe-1.csv", on_bad_lines="skip")
-    try:
-        fig2 = px.histogram(df, x="'Age'", y=input_value, hover_data=hover_data)
-    except:
-        pass
-    return fig2
+def update_bar_chart(input_value,filter_nationality,filter_club):
+    df = pd.read_csv("ivda/01/data/Aufgabe-1.csv", on_bad_lines="skip",low_memory=False)
+    color = None
+    if filter_nationality:
+        df = df[df["\'Nationality\'"].isin(filter_nationality)]
+        color="'Nationality'"
+    if filter_club:
+        df = df[df["\'Club Name\'"].isin(filter_club)]
+        color="'Club Name'"
+    if not input_value:
+        raise PreventUpdate
+    return px.histogram(df, x="'Age'", y=input_value, hover_data=hover_data,color=color)
 
 
 app.run_server(debug=True)
 
 
 """
+@app.callback(
+    Output("filt_key", "filter_option"),
+    Input("filt_key", "search_value")
+)
+def update_filter_options(search_value):
+    if not search_value:
+        raise PreventUpdate
+    return [o for o in filter_option if search_value in o["label"]]
+
+@app.callback(
+    Output("filter_value", "options"),
+    Input("filt_key", "attr_value"),
+    Input("filter_value", "search_value")
+)
+def update_options(search_value,attr_value):
+   # if not search_value:
+   #     raise PreventUpdate
+   # options = 
+    #return [o for o in options if search_value in o]
+    return df[attr_value].tolist()
+"""
+"""
 app.layout = html.Div([
     html.H4('Interactive scatter plot with the dataset'),
-    dcc.RadioItems(options=["\'Wage(in Euro)\'", '\'Overall\''], value="\'Overall\'",id='controls-and-radio-item'),
+    dcc.RadioItems(filter_options=["\'Wage(in Euro)\'", '\'Overall\''], value="\'Overall\'",id='controls-and-radio-item'),
     dcc.Graph(id="scatter-plot"),
     html.P("Filter by Age:"),
     dcc.RangeSlider(
