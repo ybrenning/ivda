12275cc1b23c7effa788867817194865e87cc733
ybrenning <yannickbrenning2@gmail.com>
2024-01-19 12:21:13 +0100

    Update learning curves

--- 04/scripts/learning_curves.py
+++ 04/scripts/learning_curves.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
-
-from sklearn.preprocessing import scale
-from sklearn.model_selection import train_test_split, learning_curve
+from sklearn.model_selection import learning_curve, train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import scale
 
-df = pd.read_csv('data/preprocessed.csv')
+df = pd.read_csv('../data/preprocessed.csv')
 
 X = df[df.columns[:-1]].to_numpy()
 y = df['target_class'].to_numpy()
@@ -21,10 +20,10 @@ if __name__ == "__main__":
     mlp1 = MLPClassifier(hidden_layer_sizes=(4,), max_iter=1000, random_state=42)
     mlp2 = MLPClassifier(hidden_layer_sizes=(16, 16,), max_iter=1000, random_state=42)
     train_size_abs, train_scores, test_scores = learning_curve(mlp1, X_scaled, y, train_sizes=np.linspace(0.1, 1.0, 10))
-    np.save('train_abs_1.npy', train_size_abs)
-    np.save('train_1.npy', train_scores)
-    np.save('test_1.npy', test_scores)
+    np.save('../assets/train_abs_1.npy', train_size_abs)
+    np.save('../assets/train_1.npy', train_scores)
+    np.save('../assets/test_1.npy', test_scores)
     train_size_abs, train_scores, test_scores = learning_curve(mlp2, X_scaled, y, train_sizes=np.linspace(0.1, 1.0, 10))
-    np.save('train_abs_2.npy', train_size_abs)
-    np.save('train_2.npy', train_scores)
-    np.save('test_2.npy', test_scores)
+    np.save('../assets/train_abs_2.npy', train_size_abs)
+    np.save('../assets/train_2.npy', train_scores)
+    np.save('../assets/test_2.npy', test_scores)
